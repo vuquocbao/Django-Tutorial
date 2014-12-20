f6376d6c5b4ea8345acaaf8f3c4ec0a1b40325ad
QuocBao Vu <quocbao@techsmartkids.com>
2014-12-20 14:29:29 -0800

    Polls App for Admin Page

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -17,6 +17,10 @@ class Question(models.Model):
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published Recently?'
+
 class Choice(models.Model):
     question = models.ForeignKey(Question) # Link choice to question model
     choice_text = models.CharField(max_length=200) # Holds choice string
